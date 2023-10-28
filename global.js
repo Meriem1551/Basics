d9a4dedb7a25e0f8b224719f05d17d9774c39bbd
Meriem1551 <meriem200409@gmail.com>
2023-10-28 16:23:02 +0200

    Global

--- global.js
+++ global.js
@@ -1,17 +1,16 @@
 //GLOBAL OBJECT
 
-// console.log(global);
+console.log(global);
 
-// setTimeout(() => {
-//     console.log(`ìn the timeout`);
-//     clearInterval(int);
-// }, 3000);
+setTimeout(() => {
+    console.log(`ìn the timeout`);
+    clearInterval(int);
+}, 3000);
 
-// const int = setInterval(() => {
-//     console.log(`ìn the Interval`);
-// }, 1000);
+const int = setInterval(() => {
+    console.log(`ìn the Interval`);
+}, 1000);
 
-// console.log(__dirname);
-// console.log(__filename);
+console.log(__dirname);
+console.log(__filename);
 
-console.log(document.querySelector);
