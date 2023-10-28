7cd0337f565d7177cdd59b2056a4cc3d2727232c
Meriem1551 <meriem200409@gmail.com>
2023-10-28 16:22:46 +0200

    Global

---
+++ global.js
@@ -0,0 +1,17 @@
+//GLOBAL OBJECT
+
+// console.log(global);
+
+// setTimeout(() => {
+//     console.log(`ìn the timeout`);
+//     clearInterval(int);
+// }, 3000);
+
+// const int = setInterval(() => {
+//     console.log(`ìn the Interval`);
+// }, 1000);
+
+// console.log(__dirname);
+// console.log(__filename);
+
+console.log(document.querySelector);
