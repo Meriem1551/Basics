156fcb0f0f0c1f18159168196f93f3d6ab86867d
Meriem1551 <meriem200409@gmail.com>
2023-10-28 16:55:10 +0200

    Modules

---
+++ modules.js
@@ -0,0 +1,9 @@
+const {people, ages} = require('./people');
+
+// console.log(p.people);
+// console.log(p.ages);
+// console.log(people, ages);
+
+const os = require('os');
+
+console.log(os.platform(), os.homedir());
---
+++ people.js
@@ -0,0 +1,7 @@
+const people = ['Meriem', 'Gojo', 'Itadori', 'Hori', 'Miyamura']
+const ages = [12, 24, 34, 19, 5]
+
+module.exports = {    //p will be equal to whatever we export here 
+    people, ages
+};
+
