b64635c1dd80503809d7b967b77b1b289330d9f8
Meriem1551 <meriem200409@gmail.com>
2023-11-03 18:09:55 +0100

    Creating pipe

--- streams.js
+++ streams.js
@@ -3,12 +3,12 @@ const fs = require('fs');
 const readStream = fs.createReadStream('./docs/blog3.txt', {encoding: 'utf-8'});
 const writeStream = fs.createWriteStream('./docs/blog4.txt', {encoding: 'utf-8'});
 
-// readStream.on('data', (chunk) => {
-//     console.log('--------NEW CHUNK ------');
-//     console.log(chunk.toString());
-//     writeStream.write('\nNEW CHUNK\n');
-//     writeStream.write(chunk);
-// });
+readStream.on('data', (chunk) => {
+    console.log('--------NEW CHUNK ------');
+    console.log(chunk.toString());
+    writeStream.write('\nNEW CHUNK\n');
+    writeStream.write(chunk);
+});
 
 //PIPING
 readStream.pipe(writeStream);
