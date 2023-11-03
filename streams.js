b20a94a5af677e756944dd6275783094e4996ef5
Meriem1551 <meriem200409@gmail.com>
2023-11-03 18:05:58 +0100

    Creating write stream

--- streams.js
+++ streams.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
 
 const readStraem = fs.createReadStream('./docs/blog3.txt', {encoding: 'utf-8'});
+const writeStraem = fs.createWriteStream('./docs/blog4.txt', {encoding: 'utf-8'});
 
 readStraem.on('data', (chunk) => {
     console.log('--------NEW CHUNK ------');
     console.log(chunk.toString());
+    writeStraem.write('\nNEW CHUNK\n');
+    writeStraem.write(chunk);
 });
